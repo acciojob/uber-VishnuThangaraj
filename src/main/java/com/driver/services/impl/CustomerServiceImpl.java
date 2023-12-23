2d93ac1d2e351b92660cbe629fa29082dc67134e
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-12-23 12:09:32 +0530

    first

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -1,16 +1,15 @@
 package com.driver.services.impl;
 
-import com.driver.model.TripBooking;
+import com.driver.model.*;
 import com.driver.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.driver.model.Customer;
 import com.driver.repository.CustomerRepository;
 import com.driver.repository.DriverRepository;
 import com.driver.repository.TripBookingRepository;
-import com.driver.model.TripStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -41,6 +40,23 @@ public class CustomerServiceImpl implements CustomerService {
 	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception{
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
+		List<Driver> drivers = driverRepository2.findAll();
+		Driver rider = null;
+		int id = Integer.MAX_VALUE;
+		for(Driver driver : drivers){
+			if(driver.getDriverId() < id){
+				Cab c = driver.getCab();
+				if(c.getAvailable()){
+					id = driver.getDriverId();
+					rider = driver;
+				}
+			}
+		}
+
+		if(rider == null){
+			throw new Exception("No cab available!");
+		}
+
 		Optional<Customer> c = customerRepository2.findById(customerId);
 		Customer customer = c.get();
 
@@ -50,7 +66,9 @@ public class CustomerServiceImpl implements CustomerService {
 		trip.setToLocation(toLocation);
 		trip.setCustomer(customer);
 		trip.setDistanceInKm(distanceInKm);
-
+		trip.setDriver(rider);
+		trip.setBill(trip.getDistanceInKm() * rider.getCab().getPerKmRate());
+		trip.setStatus(TripStatus.CONFIRMED);
 
 		return trip;
 	}
